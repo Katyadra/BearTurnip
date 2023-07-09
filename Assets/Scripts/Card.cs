d5bbd9298f3b9f84b1e9182f27bafa7768ab6205
MakeevaD <138890365+MakeevaD@users.noreply.github.com>
2023-07-09 12:27:43 +0300

    r

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,54 +1,55 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Card
+public class Card : MonoBehaviour
 {
-    public string name;
-    public string country;
-    public bool isSpecial;
+    public List<Card> Deck;
+    public List<Card> Hand;
+    public List<Card> Player1Cards;
 
-    public Card(string name, string country, bool isSpecial)
+    public void DealCards()
     {
-        this.name = name;
-        this.country = country;
-        this.isSpecial = isSpecial;
+        // Перетасовка колоды
+        ShuffleDeck();
+
+        // Очистка рук игроков перед раздачей
+        Hand.Clear();
+        Player1Cards.Clear();
+
+        // Раздача карт между игроками
+        int cardsPerPlayer = 3; // Количество карт для каждого игрока
+
+        for (int i = 0; i < cardsPerPlayer; i++)
+        {
+            // Выбор случайной карты из колоды
+            int randomIndex = Random.Range(0, Deck.Count);
+            Card card = Deck[randomIndex];
+
+            // Перемещение карты из колоды в руку первого игрока
+            Hand.Add(card);
+            Deck.RemoveAt(randomIndex);
+
+            // Выбор следующей случайной карты из колоды
+            randomIndex = Random.Range(0, Deck.Count);
+            card = Deck[randomIndex];
+
+            // Перемещение карты из колоды в руку второго игрока
+            Player1Cards.Add(card);
+            Deck.RemoveAt(randomIndex);
+        }
+    }
+
+    private void ShuffleDeck()
+    {
+        // Алгоритм перетасовки колоды
+        for (int i = 0; i < Deck.Count; i++)
+        {
+            Card temp = Deck[i];
+            int randomIndex = Random.Range(i, Deck.Count);
+            Deck[i] = Deck[randomIndex];
+            Deck[randomIndex] = temp;
+        }
     }
 }
 
-public static class CardManager
-{
-    public static List<Card> AllCards = new List<Card>();
-    
-}
-
-public class CardManagerScr : MonoBehaviour
-{
-    public void Awake()
-    {
-        CardManager.AllCards.Add(new Card("Babka", "Russia", false));
-        CardManager.AllCards.Add(new Card("Ded", "Russia", false));
-        CardManager.AllCards.Add(new Card("Vnuchka", "Russia", false));
-        CardManager.AllCards.Add(new Card("Dog", "Russia", false));
-        CardManager.AllCards.Add(new Card("Cat", "Russia", false));
-        CardManager.AllCards.Add(new Card("Mouse", "Russia", false));
-        CardManager.AllCards.Add(new Card("Babka", "Usa", false));
-        CardManager.AllCards.Add(new Card("Ded", "Usa", false));
-        CardManager.AllCards.Add(new Card("Vnuchka", "Usa", false));
-        CardManager.AllCards.Add(new Card("Dog", "Usa", false));
-        CardManager.AllCards.Add(new Card("Cat", "Usa", false));
-        CardManager.AllCards.Add(new Card("Mouse", "Usa", false));
-        CardManager.AllCards.Add(new Card("Babka", "England", false));
-        CardManager.AllCards.Add(new Card("Ded", "England", false));
-        CardManager.AllCards.Add(new Card("Vnuchka", "England", false));
-        CardManager.AllCards.Add(new Card("Dog", "England", false));
-        CardManager.AllCards.Add(new Card("Cat", "England", false));
-        CardManager.AllCards.Add(new Card("Mouse", "England", false));
-        CardManager.AllCards.Add(new Card("Babka", "Italy", false));
-        CardManager.AllCards.Add(new Card("Ded", "Italy", false));
-        CardManager.AllCards.Add(new Card("Vnuchka", "Italy", false));
-        CardManager.AllCards.Add(new Card("Dog", "Italy", false));
-        CardManager.AllCards.Add(new Card("Cat", "Italy", false));
-        CardManager.AllCards.Add(new Card("Mouse", "Italy", false));
-    }
-}
--- Assets/Scripts/CardHandler.cs
+++ Assets/Scripts/CardHandler.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardHandler : MonoBehaviour, IDropHandler
+public class CardHandler : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
         DragAndDrop card = eventData.pointerDrag.GetComponent<DragAndDrop>();
         if (card)
             card.DefaultParent = transform;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+        DragAndDrop card = eventData.pointerDrag.GetComponent<DragAndDrop>();
+        if (card)
+            card.DefaultTempCardParent = transform;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+        DragAndDrop card = eventData.pointerDrag.GetComponent<DragAndDrop>();
+        if (card && card.DefaultTempCardParent==transform)
+            card.DefaultTempCardParent = card.DefaultParent;
     }
 }
