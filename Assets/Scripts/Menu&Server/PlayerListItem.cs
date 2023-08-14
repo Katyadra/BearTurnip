d620217cf49fe3d29e030ba0940d50f3edcaab71
kruasann <117371197+XuMuK57@users.noreply.github.com>
2023-08-14 17:38:20 +0300

    Add Photon connecting

---
+++ Assets/Scripts/Menu&Server/PlayerListItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerListItem : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private TMP_Text playerName;
+    private Player _player;
+
+    public void SetUp(Player player)
+    {
+        _player = player;
+        playerName.text = player.NickName;
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        if (_player == otherPlayer)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        Destroy(gameObject);
+    }
+}
